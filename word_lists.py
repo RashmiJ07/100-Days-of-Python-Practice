50354a3a798c02bf39274aac85515d3c5622e0fc
RashmiJ07 <107768258+RashmiJ07@users.noreply.github.com>
2024-10-25 23:18:04 +0530

    Add files via upload

---
+++ word_lists.py
@@ -0,0 +1,41 @@
+# Easy words
+easy_words = [
+    "cat",
+    "dog",
+    "sun",
+    "hat",
+    "fish",
+    "tree",
+    "star",
+    "book",
+    "ball",
+    "duck"
+]
+
+# Medium words
+medium_words = [
+    "apple",
+    "monkey",
+    "flower",
+    "chair",
+    "orange",
+    "guitar",
+    "window",
+    "cloud",
+    "rocket",
+    "jelly"
+]
+
+# Hard words
+hard_words = [
+    "elephant",
+    "butterfly",
+    "chocolate",
+    "crocodile",
+    "universe",
+    "knowledge",
+    "adventure",
+    "mysterious",
+    "hieroglyphics",
+    "laboratory"
+]
